97e6be1107be2d43acd411b4461192d820a5f7f6
Kristi Green <kngreen40@gmail.com>
2017-06-26 17:22:11 -0600

    Completed  iteration-0  of testing

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,15 @@
-export default class DistrictRepository {
+import kinderData from '../data/kindergartners_in_full_day_program.js';
 
+export default class DistrictRepository {
+  constructor (kinderData) {
+    this.data = kinderData.reduce((obj, state, i, arr) => {
+
+        if(!obj[state.Location]){
+          obj[state.Location] = [state]
+        } else {
+          obj[state.Location].push(state)
+        }
+      return obj;
+    },{})
+  }
 }
